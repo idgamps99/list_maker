daf96aff1ea732811a441d6590a4a2c39c3834d6
Myles Amphlett <mylesamphlett@gmail.com>
2025-03-24 17:39:52 +0000

    get lists and get albums for specific list

--- app.rb
+++ app.rb
@@ -1,1 +1,25 @@
 require "sinatra"
+require "sqlite3"
+require "json"
+
+DB = SQLite3::Database.new "lists"
+DB.results_as_hash = true
+
+get "/lists" do
+  content_type :json
+  lists = DB.execute("SELECT * FROM lists")
+  lists.to_json
+end
+
+get "/lists/:list_id/albums" do
+  albums = DB.execute("SELECT * FROM albums WHERE list_id = ?", params["list_id"])
+  albums.to_json
+end
+
+# post "/lists" do
+
+# end
+
+# post "/lists/:list_id/albums"
+
+# end
